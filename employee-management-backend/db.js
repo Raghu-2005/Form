20ed5d941a918d747029fbcf56584827635e3866
Raghuram L <lraghuram14@gmail.com>
2024-12-17 09:24:52 +0530

    Initial commit

---
+++ employee-management-backend/db.js
@@ -0,0 +1,6 @@
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234", 
+    database: "employee_management",
+});
---
+++ employee-management-system/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EmployeeForm from "./components/EmployeeForm";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <EmployeeForm />
+    </div>
+  );
+};
+
+export default App;
+
